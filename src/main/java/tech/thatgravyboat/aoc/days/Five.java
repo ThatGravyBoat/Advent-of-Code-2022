07e2efc0ea333b2582ba95660047f58913cef080
ThatGravyBoat <thatgravyboat@gmail.com>
2022-12-05 03:34:47 -0330

    Clean up day five and use a matcher instead of replaces and splits.

--- src/main/java/tech/thatgravyboat/aoc/days/Five.java
+++ src/main/java/tech/thatgravyboat/aoc/days/Five.java
@@ -3,8 +3,11 @@ package tech.thatgravyboat.aoc.days;
 import tech.thatgravyboat.aoc.templates.Template;
 
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Five extends Template {
 
@@ -12,24 +15,23 @@ public class Five extends Template {
         new Five().load(5);
     }
 
+    private static final Pattern MOVE_PATTERN = Pattern.compile("move (\\d+) from (\\d+) to (\\d+)");
+
     private final List<Move> moves = new ArrayList<>();
 
     @Override
     protected void onInputLoaded() {
         for (String s : getInput()) {
-            if (!s.startsWith("move")) continue;
-            String[] split = s.replaceAll("move ", "")
-                    .replace("from ", "")
-                    .replace("to ", "")
-                    .split(" ");
-            moves.add(new Move(Integer.parseInt(split[0]), Integer.parseInt(split[1]), Integer.parseInt(split[2])));
+            Matcher matcher = MOVE_PATTERN.matcher(s);
+            if (!matcher.find()) continue;
+            moves.add(new Move(Integer.parseInt(matcher.group(1)), Integer.parseInt(matcher.group(2)), Integer.parseInt(matcher.group(3))));
         }
     }
 
     @Override
     public String partOne() {
         List<Stack<Character>> stacks = getInitialStack();
-        moves.forEach(move -> IntStream.range(0, move.amount).forEachOrdered(i -> move.getTo(stacks).push(move.getFrom(stacks).pop())));
+        moves.forEach(move -> IntStream.range(0, move.amount).forEachOrdered(i -> move.push(stacks, move.pop(stacks))));
         return stacks.stream()
             .map(Stack::pop)
             .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
@@ -40,12 +42,12 @@ public class Five extends Template {
     public String partTwo() {
         List<Stack<Character>> stacks = getInitialStack();
         moves.forEach(move -> IntStream.range(0, move.amount)
-            .mapToObj(i -> move.getFrom(stacks).pop())
+            .mapToObj(i -> move.pop(stacks))
             .collect(Collectors.collectingAndThen(Collectors.toList(), list -> {
                 Collections.reverse(list);
                 return list;
             }))
-            .forEach(c -> move.getTo(stacks).push(c))
+            .forEach(c -> move.push(stacks, c))
         );
         return stacks.stream()
                 .map(Stack::pop)
@@ -87,12 +89,12 @@ public class Five extends Template {
 
     public record Move(int amount, int from, int to) {
 
-        public <T> Stack<T> getFrom(List<Stack<T>> stacks) {
-            return stacks.get(from - 1);
+        public <T> T pop(List<Stack<T>> stacks) {
+            return stacks.get(from - 1).pop();
         }
 
-        public <T> Stack<T> getTo(List<Stack<T>> stacks) {
-            return stacks.get(to - 1);
+        public <T> void push(List<Stack<T>> stacks, T object) {
+            stacks.get(to - 1).push(object);
         }
     }
 }
