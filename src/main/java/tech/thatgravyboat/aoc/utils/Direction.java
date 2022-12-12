54331b1ba94fa6aa805f7d056fa0759e3b6e7fd3
ThatGravyBoat <thatgravyboat@gmail.com>
2022-12-12 05:07:23 -0330

    Fix dir

--- src/main/java/tech/thatgravyboat/aoc/utils/Direction.java
+++ src/main/java/tech/thatgravyboat/aoc/utils/Direction.java
@@ -1,8 +1,8 @@
 package tech.thatgravyboat.aoc.utils;
 
 public enum Direction {
-    UP(new Vec2i(0, 1)),
-    DOWN(new Vec2i(0, -1)),
+    UP(new Vec2i(0, -1)),
+    DOWN(new Vec2i(0, 1)),
     LEFT(new Vec2i(-1, 0)),
     RIGHT(new Vec2i(1, 0));
 
